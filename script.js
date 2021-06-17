1110b3473d60d1779d82769c9ff920447dad79fe
tornedoboy <tornedoboy@gmail.com>
2021-06-17 14:17:42 -0700

    working on it

--- script.js
+++ script.js
@@ -9,19 +9,21 @@ function grid() {
     scr();
 
     function scr() {
-        document.getElementById("id1").style.gridTemplateColoumns = `repeat(${screensize}, auto)`;
-        document.getElementById("id1").style.gridTemplateRows = `repeat(${screensize}, auto)`;
+
         for (let i = 0; i < screensize ** 2; i++) {
             console.log(screensize)
             let div = document.createElement('div');
             div.classList.add('divs');
+            document.getElementById("id1").style.backgroundColor = 'white';
+
 
             document.getElementById("id1").appendChild(div);
-            // document.getElementById("id1").style.setProperty("--row", div);
-            // document.getElementById("id1").style.setProperty("--col", div);
+
 
 
         }
+        document.getElementById("id1").style.gridTemplateColoumns = `repeat(${screensize}, auto)`;
+        document.getElementById("id1").style.gridTemplateRows = `repeat(${screensize}, auto)`;
 
 
 
