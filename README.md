a619d9d54c69f6e17abb7e3dbff0759e61613a08
tornedoboy <tornedoboy@gmail.com>
2021-06-17 13:37:23 -0700

    working on grid

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # etch-a-sketch
+# I have tried my best hope you enjoy
---
+++ script.js
@@ -0,0 +1,32 @@
+function grid() {
+    const button = document.querySelectorAll('.btn');
+
+    const screen = document.querySelectorAll('.screen');
+
+    //let pixel = "";
+    let gridsize = 64;
+    let screensize = 6;
+    scr();
+
+    function scr() {
+        document.getElementById("id1").style.gridTemplateColoumns = `repeat(${screensize}, auto)`;
+        document.getElementById("id1").style.gridTemplateRows = `repeat(${screensize}, auto)`;
+        for (let i = 0; i < screensize ** 2; i++) {
+            console.log(screensize)
+            let div = document.createElement('div');
+            div.classList.add('divs');
+
+            document.getElementById("id1").appendChild(div);
+            // document.getElementById("id1").style.setProperty("--row", div);
+            // document.getElementById("id1").style.setProperty("--col", div);
+
+
+        }
+
+
+
+    }
+
+
+
+}
